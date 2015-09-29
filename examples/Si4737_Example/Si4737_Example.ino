260229286480e05ee3f068042157c04d3945fe4f
Sean T. McBeth <sean.mcbeth@gmail.com>
2015-09-29 13:21:06 -0400

    delete unused code

--- examples/Si4737_Example/Si4737_Example.ino
+++ examples/Si4737_Example/Si4737_Example.ino
@@ -15,25 +15,15 @@ Send command 0x11: "c11w"
 //UWAFT Si4737 I2C Functions
 #include <Si4737_i2c.h>
 
-// Setup address and reset pin
-#define _i2caddr 0x11
-#define mcuradio_rst 9
-
-#define POWER_UP 0x01
-#define FM_SEEK_START 0x21
-
 //Create our Si4737 radio
 Si4737 radio;
 
 //Other variables we will use below
 byte response[16], command[8], numDigits, status, mode;
 char collectedDigits[17] = "", serial_in, oneHexValue[5] = "0x";
-word frequency, rdsblock[4];
+word frequency;
 bool goodtune;
 Si4737_RX_Metrics RSQ;
-Si4737_RDS_Data station;
-Si4737_RDS_Time rdstime;
-char FW[3], REV;
 
 void setup() {
     // NOTE: if you're running on a 3.3v Arduino, you should double the baud rate
@@ -43,19 +33,8 @@ void setup() {
     // speed, which screws up the timing.
     Serial.begin(115200*2);
 
-    /*
-    pinMode(mcuradio_rst, OUTPUT); // Set reset to output
-    digitalWrite(mcuradio_rst, LOW); // Put device in reset mode and wait a little
-    delay(1);
-    digitalWrite(mcuradio_rst, HIGH); // Bring device out of reset and wait for it to be ready
-    delay(1);
-    Serial.println("Start I2C Mode");
-    Wire.begin();                     // Start I2C
-    */
     radio.begin(SI4737_MODE_FM);
-
     Serial.println("ino code - Radio Powered up, FM mode");
-    //radio.sendCommand(SI4737_CMD_POWER_UP, 0xC0, 0xB5);  // Send Power-up command, enable interrupts, set external clock source, set device to FM mode, enable digital and analog audio outputs.
     delay(100);                          // Wait for Boot 
 
     radio.seekUp();
@@ -103,8 +82,6 @@ void loop()
             clcCmd();
             break;
         case 'V': //Set Volume, 0 to 63
-                  //Serial.print("Set Volume to ");
-                  //Serial.print(atoi(collectedDigits));
             radio.setVolume(atoi(collectedDigits));
             clcCmd();
             break;
@@ -197,7 +174,6 @@ void loop()
                         Serial.print(serial_in);
                     }
                     else Serial.println(F("Too many hex digits, 16 maximum!"));
-                    //else Serial.println(F("Invalid command!"));
                     Serial.flush();
             }
 
@@ -244,10 +220,6 @@ void loop()
             //If we get any other character
             //copy it to the command string.
         default:
-            /*Serial.print("serial_in=");
-            Serial.print(serial_in);
-            Serial.print("\n");
-            */
             strncat(collectedDigits, &serial_in, 1);
             //Echo the command string as it is composed
             Serial.print(serial_in);
@@ -256,14 +228,6 @@ void loop()
         }
     }
 }
-
-//void seekup() {
-//	Wire.beginTransmission(_i2caddr); 
-//	Wire.write(FM_SEEK_START);      
-//	Wire.write(0xC);
-//	Wire.endTransmission();   
-//	delay(100);
-//}
 
 void clcCmd() {
     Serial.flush();
