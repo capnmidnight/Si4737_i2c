1563e66b76bfb00ddf97994d0ed6bb6c315a3c44
Sean T. McBeth <sean.mcbeth@gmail.com>
2015-09-29 09:17:34 -0400

    Basic raw I2C example

---
+++ examples/BasicSi4737_I2CExample/BasicSi4737_I2CExample.ino
@@ -0,0 +1,98 @@
+/*
+Name:		BasicSi4737_I2CExample.ino
+Created:	9/28/2015 3:00:31 PM
+Author:	Sean
+*/
+
+#include <Wire.h>
+
+#define POWER_PIN A0
+#define SI4737_BUS_ADDRESS 0x11
+#define SI4737_PIN_RESET A3
+#define SI4737_PIN_SDIO A4
+#define SI4737_PIN_SCLK A5
+
+void setup()
+{
+    Serial.begin(115200 * 2);
+    Wire.begin();
+    Serial.println();
+    Serial.println();
+
+    Serial.println("Si4737 setup sequence:");
+
+    Serial.print("Set RSTb LOW, set power HIGH... ");
+    pinMode(SI4737_PIN_RESET, OUTPUT);
+    pinMode(POWER_PIN, OUTPUT);
+    digitalWrite(SI4737_PIN_RESET, LOW);
+    digitalWrite(POWER_PIN, HIGH);
+    Serial.println("OK");
+
+    Serial.print("Wait 250us for VDD and VIO to stablize... ");
+    delay(250);
+    Serial.println("OK");
+
+    Serial.print("Use default bus mode and wait 100us to stabalize... ");
+    delay(100);
+    Serial.println("OK");
+
+    Serial.print("Set RESTb HIGH and wait at least 10ns... ");
+    digitalWrite(SI4737_PIN_RESET, HIGH);
+    delay(1);
+    Serial.println("OK");
+    Serial.println("Si4737 setup sequence complete!");
+    Serial.println();
+
+    Serial.print("Writing POWER_UP command... ");
+    Wire.beginTransmission(SI4737_BUS_ADDRESS);
+    Wire.write(0x01); // POWER_UP
+    Wire.write(0xC0); // HIGH[XOSCEN], LOW[FM_RECV]
+    Wire.write(0x50); // ANALOG_OUT
+    byte status = Wire.endTransmission(false);
+    Serial.print("I2C status: ");
+    Serial.print(status);
+    Serial.print(" -> ");
+    switch (status)
+    {
+    case 0: Serial.print("OK"); break;
+    case 1: Serial.print("ERR: data too long to fit in transmit buffer."); break;
+    case 2: Serial.print("ERR: received NACK on transmit of address."); break;
+    case 3: Serial.print("ERR: received NACK on transmit of data."); break;
+    default: Serial.print("ERR: unknown."); break;
+    }
+    Serial.println();
+
+    if (status == 0)
+    {
+        bool clear = false;
+        while (!clear)
+        {
+            Serial.print("Retrieve clear-to-send status..");
+            Wire.requestFrom(SI4737_BUS_ADDRESS, 1);
+            while (!Wire.available())
+            {
+                Serial.print('.');
+                delay(10);
+            }
+            status = Wire.read();
+            Serial.print(' ');
+            Serial.print(status, 16);
+            Serial.print(" -> ");
+            if (status & 0x80)
+            {
+                Serial.print("clear-to-send ");
+                clear = true;
+            }
+            if (status & 0x40)
+            {
+                Serial.print("ERROR ");
+            }
+            Serial.println();
+        }
+    }
+}
+
+void loop()
+{
+}
+
