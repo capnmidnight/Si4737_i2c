6417326b20711ea07c83beea141d96837ec1c492
Sean T. McBeth <sean.mcbeth@gmail.com>
2015-10-01 00:46:01 -0400

    interrupt-based solution

--- examples/BasicSi4737_I2CExample/BasicSi4737_I2CExample.ino
+++ examples/BasicSi4737_I2CExample/BasicSi4737_I2CExample.ino
@@ -6,26 +6,43 @@ Author:	Sean
 
 #include <Wire.h>
 
-#define POWER_PIN A0
 #define SI4737_BUS_ADDRESS 0x11
-#define SI4737_PIN_RESET A3
-#define SI4737_PIN_SDIO A4
-#define SI4737_PIN_SCLK A5
+#define POWER_PIN 8
+#define SI4737_PIN_RESET A0
+#define CLOCK_PIN 9
 #define CLOCK_SHIFT 1
+#define ARDUINO_PWM_FREQ 490 >> CLOCK_SHIFT
+#define GPO2_INTERUPT_PIN 2
+
+#define POWERUP_CTS_INT_EN 0x80
+#define POWERUP_GPO2_EN 0x40
+#define POWERUP_PATCH  0x20
+#define POWERUP_X_OSC_EN 0x10
+#define POWERUP_FUNC_FM_RCV 0x00
+#define POWERUP_FUNC_QUERY_LIB_ID 0x0F
+
+#define POWERUP_OPMODE_RDS_ONLY 0x00
+#define POWERUP_OPMODE_ANALOG 0x05
+#define POWERUP_OPMODE_DIGITAL_ON_ANALOG_PINS 0x0B
+#define POWERUP_OPMODE_DIGITAL 0xB0
+
+
+byte ARGS[7] = { 0,0,0,0,0,0,0 };
+byte RESP[15] = { 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0 };
+
+bool ready = true, CTS = true, needCTS = false, STC = true, needSTC = false;
+int step = 0, expResp = 0;
 
 void prepareChip();
 void wait(int);
 void sleep(int);
-bool translateI2CStatus(byte);
-bool translateChipStatus(byte);
-byte getChipStatus();
-bool sendCommand(const char*, const byte, const size_t, const byte*, size_t, byte*, bool = true, bool = false);
-void printResponse(const char*, const size_t, const byte*, int = 16);
+bool sendCommand(const char*, const byte, const size_t);
+void printBuffer(const char*, const size_t, const byte*, int = 16);
+void GPO2InteruptHandler();
 
 void setup()
 {
-    Wire.begin();
-
+    attachInterrupt(digitalPinToInterrupt(GPO2_INTERUPT_PIN), GPO2InteruptHandler, FALLING);
     // NOTE: if you're running on a 3.3v Arduino, you should double the baud rate
     // at which you run the SoftSerial library, but do not double it in the Serial
     // Monitor, because the lower voltage Arduino's only run at half the clock
@@ -34,64 +51,134 @@ void setup()
     Serial.begin(115200 << CLOCK_SHIFT);
     Serial.println();
     Serial.println();
+    Serial.println("OK");
+
+    Wire.begin();
 
     prepareChip();
 
-    byte POWER_UP[] = { 0x01, 2, 1 };
-    byte GET_REV[] = { 0x10, 0, 8 };
+    //sendCommand("POWER_DOWN", 0x11, 0, 0);
+}
 
-    byte ARGS[7] = { 0,0,0,0,0,0,0 };
-    byte RESP[15] = { 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0 };
-    ARGS[0] = 0x10;
-    ARGS[1] = 0xB5;
-    if (sendCommand("POWER_UP", 0x01, 2, ARGS, 0, RESP)
-        && sendCommand("GET_REV", 0x10, 0, ARGS, 8, RESP))
+void GPO2InteruptHandler()
+{
+    ready = true;
+}
+
+void loop()
+{
+    if (ready)
     {
-        printResponse("Revision", 8, RESP);
-
-        // get volume
-        ARGS[0] = 0x00; // always 0
-        ARGS[1] = 0x40;
-        ARGS[2] = 0x00;
-        if (sendCommand("GET_PROPERTY: RX_VOLUME", 0x13, 3, ARGS, 3, RESP)) 
+        ready = false;
+        bool needStatus = needCTS || needSTC;
+        if (expResp > 0 || needStatus)
         {
-            printResponse("Volume", 3, RESP);
-
-            // set volume
-            ARGS[0] = 0x00; // always 0
-            ARGS[1] = 0x40;
-            ARGS[2] = 0x00;
-            ARGS[3] = 0x00;
-            ARGS[4] = 0x50;
-            if (sendCommand("SET_PROPERTY: RX_VOLUME", 0x12, 5, ARGS, 0, RESP, false))
+            Serial.print("... Retrieving response...");
+            Wire.requestFrom(SI4737_BUS_ADDRESS, expResp + (needStatus ? 1 : 0));
+            while (Wire.available() < expResp)
             {
-                if (sendCommand("GET_PROPERTY: RX_VOLUME", 0x13, 3, ARGS, 3, RESP))
-                {
-                    printResponse("Volume", 3, RESP);
+                Serial.print('.');
+                sleep(100);
+            }
 
-                    ARGS[0] = 0x00; // always 0
-                    ARGS[1] = (10110 &0xff00) >> 8;
-                    ARGS[2] = (10110 &0x00ff);
-                    ARGS[3] = 0x00; // automatically set the tuning cap
-                    if (sendCommand("FM_TUNE_FREQ: 101.1", 0x20, 4, ARGS, 0, RESP))
-                    {
-                        if (sendCommand("GET_INT_STATUS", 0x14, 0, ARGS, 0, RESP, true, true)) {
-                            Serial.print("Waiting a few seconds");
-                            wait(3);
-                            Serial.println(" OK");
-                            ARGS[0] = 0x00;
-                            if (sendCommand("GET_PROPERTY: FM_TUNE_STATUS", 0x13, 1, ARGS, 7, RESP))
-                            {
-                                printResponse("Tune", 7, RESP);
-                                if (sendCommand("POWER_DOWN", 0x11, 0, ARGS, 0, RESP))
-                                {
-                                    Serial.println("Successfully shut down.");
-                                }
-                            }
-                        }
-                    }
+            if (needStatus)
+            {
+                byte status = Wire.read();
+                if (needCTS && (status & 0x80) != 0)
+                {
+                    CTS = true;
+                }
+
+                if (needSTC && (status & 0x01) != 0)
+                {
+                    STC = true;
+                }
+
+                if (status & 0x80)
+                {
+                    Serial.print("CTS ");
+                }
+
+                if (status & 0x40)
+                {
+                    Serial.print("ERROR ");
+                }
+
+                if (status & 0x01)
+                {
+                    Serial.print("STC ");
                 }
             }
+
+            if (expResp > 0) 
+            {
+                Wire.readBytes(RESP, expResp);
+                printBuffer("RESP", expResp, RESP);
+                expResp = 0;
+            }
+        }
+
+        Serial.print("\nstep ");
+        Serial.print(step);
+        Serial.print(": ");
+        switch (step)
+        {
+        case 0:
+            // do the powerup shuffle
+            ARGS[0] = POWERUP_CTS_INT_EN | POWERUP_GPO2_EN | POWERUP_X_OSC_EN | POWERUP_FUNC_FM_RCV;
+            ARGS[1] = POWERUP_OPMODE_ANALOG;
+            needCTS = true;
+            sendCommand("POWER_UP", 0x01, 2);
+            break;
+
+        case 1:
+            // enable GPIO pins, enable all to avoid excessive current consumption
+            // due to ascillation.
+            ARGS[0] = 0x0E;
+            needCTS = true;
+            sendCommand("GPIO_CTL", 0x80, 1);
+            break;
+
+        case 2:
+            // Set the interupt sources
+            ARGS[0] = 0x00; // always 0
+            ARGS[1] = 0x00;
+            ARGS[2] = 0x01;
+            ARGS[3] = 0x00;
+            ARGS[4] = 0x81;
+            sendCommand("SET_PROPERTY: GPO_IEN", 0x12, 5);
+            ready = true;
+            break;
+
+        case 3:
+            // Tune to 97.1
+            ARGS[0] = 0x00; // always 0
+            ARGS[1] = (9710 & 0xff00) >> 8;
+            ARGS[2] = (9710 & 0x00ff);
+            ARGS[3] = 0x00; // automatically set the tuning cap
+            needCTS = true;
+            needSTC = true;
+            sendCommand("FM_TUNE_FREQ: 97.1", 0x20, 4);
+            break;
+
+        case 4:
+            Serial.print("Waiting a few seconds");
+            wait(3);
+            Serial.println(" OK");
+            ready = true;
+            break;
+
+        case 5:
+            ARGS[0] = 0x00;
+            expResp = 7;
+            needCTS = true;
+            sendCommand("GET_PROPERTY: FM_TUNE_STATUS", 0x13, 1);
+            break;
+
+        case 6:
+            printBuffer("Tune", 7, RESP);
+            Serial.println("All done");
+            break;
         }
     }
 }
@@ -101,104 +188,48 @@ void sleep(int millis)
     delay(millis >> CLOCK_SHIFT);
 }
 
-void wait(int seconds) 
+void wait(int seconds)
 {
-    for (int i = 0; i < seconds; ++i) 
+    for (int i = 0; i < seconds; ++i)
     {
         Serial.print('.');
         sleep(1000);
     }
 }
 
-void printResponse(const char* name, const size_t numResp, const byte* resp, int radix)
+void printBuffer(const char* name, const size_t size, const byte* buffer, int radix)
 {
     Serial.print(name);
-    Serial.print(": ");
-    for (int i = 0; i < numResp; ++i)
+    Serial.print('(');
+    if (size > 0)
     {
-        Serial.print(resp[i], radix);
-        Serial.print(' ');
+        for (int i = 0; i < size; ++i)
+        {
+            Serial.print(buffer[i], radix);
+            if (i < (size - 1))
+            {
+                Serial.print(", ");
+            }
+        }
     }
-    Serial.println();
+    Serial.println(')');
 }
 
-bool sendCommand(const char* name, const byte cmd, const size_t numArgs, const byte* args, size_t numResp, byte* resp, bool checkCTS, bool checkSTC)
+bool sendCommand(const char* name, const byte cmd, const size_t numArgs)
 {
-    bool extraByte = checkCTS || checkSTC;
-    if (extraByte) ++numResp;
-    Serial.print("Sending ");
-    Serial.print(name);
+    Serial.print("CMD ");
+    printBuffer(name, numArgs, ARGS);
     Serial.print("... ");
     Wire.beginTransmission(SI4737_BUS_ADDRESS);
     Wire.write(cmd);
     if (numArgs > 0)
     {
-        Wire.write(args, numArgs);
+        Wire.write(ARGS, numArgs);
     }
+
     byte status = Wire.endTransmission(false);
-    bool i2cGood = translateI2CStatus(status);
-    bool ctsGood = !checkCTS;
-    bool stcGood = !checkSTC;
-    // try only 3 times before giving up, we don't want to spin forever
-    for (int i = 0; i < 3 && i2cGood && (!ctsGood || !stcGood); ++i)
-    {
-        // always sleep right away, to give the chip some time to do
-        // do whatever we asked it to do.
-        sleep(100);
-        Serial.print("Retrieving response..");
-        Wire.requestFrom(SI4737_BUS_ADDRESS, numResp);
-        while (Wire.available() < numResp)
-        {
-            Serial.print('.');
-            sleep(100);
-        }
-        if (extraByte)
-        {
-            --numResp;
-            byte status = Wire.read();
-            Serial.print(' ');
-            Serial.print(status, 2);
-            Serial.print(" -> ");
 
-            if (checkCTS && (status & 0x80) != 0) 
-            {
-                ctsGood = true;
-            }
-
-            if (checkSTC && (status & 0x01) != 0)
-            {
-                stcGood = true;
-            }
-
-            if (status & 0x80)
-            {
-                Serial.print("CTS ");
-            }
-
-            if (status & 0x40)
-            {
-                Serial.print("ERROR ");
-            }
-
-            if (status & 0x01)
-            {
-                Serial.print("CMD_COMPLETE ");
-            }
-
-            Serial.println();
-        }
-        Wire.readBytes(resp, numResp);
-    }
-    return i2cGood && ctsGood && stcGood;
-}
-
-bool translateChipStatus(const byte status)
-{
-    return status == 0x80;
-}
-
-bool translateI2CStatus(const byte status)
-{
+#ifdef DEBUG_I2C
     Serial.print("I2C status: ");
     Serial.print(status);
     Serial.print(" -> ");
@@ -211,7 +242,7 @@ bool translateI2CStatus(const byte status)
     default: Serial.print("ERR: unknown."); break;
     }
     Serial.println();
-    return status == 0;
+#endif
 }
 
 void prepareChip()
@@ -225,12 +256,19 @@ void prepareChip()
     digitalWrite(POWER_PIN, HIGH);
     Serial.println("OK");
 
-    Serial.print("Wait 250us for VDD and VIO to stablize... ");
+    Serial.print("Wait 250ms for VDD and VIO to stablize... ");
     sleep(250);
     Serial.println("OK");
 
-    Serial.print("Use default bus mode and wait 100us to stabalize... ");
+    Serial.print("Use default bus mode and wait 100ms to stabalize... ");
     sleep(100);
+    Serial.println("OK");
+
+
+    Serial.print("Start the clock signal and wait 500ms for it to stabalize... ");
+    pinMode(CLOCK_PIN, OUTPUT);
+    analogWrite(CLOCK_PIN, 128);
+    sleep(500);
     Serial.println("OK");
 
     Serial.print("Set RESTb HIGH and wait at least 10ns... ");
@@ -239,9 +277,5 @@ void prepareChip()
     Serial.println("OK");
     Serial.println("Si4737 setup sequence complete!");
     Serial.println();
-}
-
-void loop()
-{
 }
 
