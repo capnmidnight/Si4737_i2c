19045bea76c1155efcba4d663c2d186a5297572f
Sean T. McBeth <sean.mcbeth@gmail.com>
2015-10-01 13:28:47 -0400

    Generate a clock via the arduino and get an actual signal!

--- examples/BasicSi4737_I2CExample/BasicSi4737_I2CExample.ino
+++ examples/BasicSi4737_I2CExample/BasicSi4737_I2CExample.ino
@@ -6,19 +6,25 @@ Author:	Sean
 
 #include <Wire.h>
 
+// change this to 0 if running at 5v, 1 if running at 3.3v
+#define CLOCK_SHIFT_5V 0
+#define CLOCK_SHIFT_3_3V 1
+#define CLOCK_SHIFT CLOCK_SHIFT_3_3V
+#define BASE_CLOCK 16000000
+#define REAL_CLOCK BASE_CLOCK >> CLOCK_SHIFT
+
 #define SI4737_BUS_ADDRESS 0x11
 #define POWER_PIN 8
 #define SI4737_PIN_RESET A0
+#define GPO2_INTERUPT_PIN 3
 #define CLOCK_PIN 9
-#define CLOCK_SHIFT 1
-#define ARDUINO_PWM_FREQ 490 >> CLOCK_SHIFT
-#define GPO2_INTERUPT_PIN 2
 
 #define POWERUP_CTS_INT_EN 0x80
 #define POWERUP_GPO2_EN 0x40
 #define POWERUP_PATCH  0x20
 #define POWERUP_X_OSC_EN 0x10
 #define POWERUP_FUNC_FM_RCV 0x00
+#define POWERUP_FUNC_WB_RCV 0x03
 #define POWERUP_FUNC_QUERY_LIB_ID 0x0F
 
 #define POWERUP_OPMODE_RDS_ONLY 0x00
@@ -31,7 +37,7 @@ byte ARGS[7] = { 0,0,0,0,0,0,0 };
 byte RESP[15] = { 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0 };
 
 bool ready = true, CTS = true, needCTS = false, STC = true, needSTC = false;
-int step = -1, expResp = 0;
+int step = -1, expResp = 0, clockPinState = LOW;
 
 void prepareChip();
 void wait(int);
@@ -42,10 +48,11 @@ void GPO2InteruptHandler();
 void getStatus();
 void advanceStep();
 void executeStep();
+int translateWB(int);
+void timer1_setup(byte, int, byte, byte, byte);
 
 void setup()
 {
-    attachInterrupt(digitalPinToInterrupt(GPO2_INTERUPT_PIN), GPO2InteruptHandler, FALLING);
     // NOTE: if you're running on a 3.3v Arduino, you should double the baud rate
     // at which you run the SoftSerial library, but do not double it in the Serial
     // Monitor, because the lower voltage Arduino's only run at half the clock
@@ -56,11 +63,11 @@ void setup()
     Serial.println();
     Serial.println("OK");
 
-    Wire.begin();
-
     prepareChip();
 
-    //sendCommand("POWER_DOWN", 0x11, 0, 0);
+    attachInterrupt(digitalPinToInterrupt(GPO2_INTERUPT_PIN), GPO2InteruptHandler, FALLING);
+
+    Wire.begin();
 }
 
 void GPO2InteruptHandler()
@@ -128,7 +135,6 @@ void getStatus()
         {
             Wire.readBytes(RESP, expResp);
             printBuffer("RESP", expResp, RESP);
-            expResp = 0;
         }
     }
 }
@@ -148,10 +154,12 @@ void advanceStep()
     CTS = false;
     needSTC = false;
     STC = false;
+    expResp = 0;
 }
 
 void executeStep()
 {
+    int f;
     Serial.print("\nstep ");
     Serial.print(step);
     Serial.print(": ");
@@ -159,17 +167,17 @@ void executeStep()
     {
     case 0:
         // do the powerup shuffle
+        needCTS = true;
         ARGS[0] = POWERUP_CTS_INT_EN | POWERUP_GPO2_EN | POWERUP_FUNC_FM_RCV;
         ARGS[1] = POWERUP_OPMODE_ANALOG;
-        needCTS = true;
         sendCommand("POWER_UP", 0x01, 2);
         break;
 
     case 1:
         // enable GPIO pins, enable all to avoid excessive current consumption
         // due to ascillation.
-        ARGS[0] = 0x0E;
         needCTS = true;
+        ARGS[0] = 0x0E;
         sendCommand("GPIO_CTL", 0x80, 1);
         break;
 
@@ -179,71 +187,144 @@ void executeStep()
         ARGS[1] = 0x00;
         ARGS[2] = 0x01;
         ARGS[3] = 0x00;
-        ARGS[4] = 0x81;
+        ARGS[4] = 0xC9;
         sendCommand("SET_PROPERTY: GPO_IEN", 0x12, 5);
         // always delay 10ms after SET_PROPERTY. It has no CTS response,
         // but the 10ms is guaranteed.
-        delay(10); 
+        delay(10);
         ready = true;
         break;
 
     case 3:
-        // Get current tuning
-        ARGS[0] = 0x01;
-        expResp = 7;
-        needCTS = true;
-        sendCommand("FM_TUNE_STATUS", 0x22, 1);
+        // Set the reference clock prescale
+        ARGS[0] = 0x00; // always 0
+        ARGS[1] = 0x02;
+        ARGS[2] = 0x02;
+        ARGS[3] = 0x00;
+        ARGS[4] = 0x01;
+        sendCommand("SET_PROPERTY: REFCLK_PRESCALE", 0x12, 5);
+        // always delay 10ms after SET_PROPERTY. It has no CTS response,
+        // but the 10ms is guaranteed.
+        delay(10);
+        ready = true;
         break;
 
     case 4:
-        // Tune to 97.1
+        // Set the reference clock frequency
         ARGS[0] = 0x00; // always 0
-        ARGS[1] = (9710 & 0xff00) >> 8;
-        ARGS[2] = (9710 & 0x00ff);
-        ARGS[3] = 0x00; // automatically set the tuning cap
-        needCTS = true;
-        sendCommand("FM_TUNE_FREQ: 97.1", 0x20, 4);
+        ARGS[1] = 0x02;
+        ARGS[2] = 0x01;
+        ARGS[3] = (31250 & 0xFF00) >> 8;
+        ARGS[4] = (31250 & 0xFF);
+        sendCommand("SET_PROPERTY: REFCLK_FREQ", 0x12, 5);
+        // always delay 10ms after SET_PROPERTY. It has no CTS response,
+        // but the 10ms is guaranteed.
+        delay(10);
+        ready = true;
         break;
 
     case 5:
+        sleep(500);
+        // Get current tuning
+        needCTS = true;
+        ARGS[0] = 0x01;
+        //expResp = 5;
+        //sendCommand("WB_TUNE_STATUS", 0x52, 1);
+        expResp = 7;
+        sendCommand("FM_TUNE_STATUS", 0x22, 1);
+        break;
+
+    case 6:
+        Serial.print("Station: ");
+        Serial.println(highByte(RESP[0]) | RESP[1]);
+        needCTS = true;
+        // Tune to WB 162.450
+        //f = translateWB(162450);
+        // Tune to FM 98.70
+        f = 9870;
+        ARGS[0] = 0x00; // always 0
+        ARGS[1] = (f & 0xff00) >> 8;
+        ARGS[2] = (f & 0x00ff);
+        //sendCommand("WB_TUNE_FREQ: 162.450", 0x50, 3);
+        ARGS[3] = 0x00; // automatically set the tuning cap
+        sendCommand("FM_TUNE_FREQ: 98.7", 0x20, 4);
+        break;
+
+    case 7:
         // Check interupt status
         needCTS = true;
         needSTC = true;
         sendCommand("GET_INT_STATUS", 0x14, 0);
         break;
 
-    case 6:
+    case 8:
+        needCTS = true;
+        ARGS[0] = 0x01;
+        expResp = 7;
+        sendCommand("FM_TUNE_STATUS", 0x22, 1);
+        break;
+
+    case 9:
+        Serial.print("Station: ");
+        Serial.println((((int)RESP[1]) << 8) | RESP[2]);
         Serial.print("Waiting a few seconds");
-        wait(3);
+        wait(5);
         Serial.println(" OK");
         ready = true;
         break;
 
-    case 7:
-        ARGS[0] = 0x00;
-        expResp = 7;
+    case 10:
+        // seek up
         needCTS = true;
-        sendCommand("GET_PROPERTY: FM_TUNE_STATUS", 0x13, 1);
+        ARGS[0] = 0x08;
+        sendCommand("FM_SEEK_START: UP", 0x21, 1);
         break;
 
-    case 8:
-        printBuffer("Tune", 7, RESP);
+    case 11:
+        // Check interupt status
+        needCTS = true;
+        needSTC = true;
+        sendCommand("GET_INT_STATUS", 0x14, 0);
+        break;
+
+    case 12:
+        needCTS = true;
+        ARGS[0] = 0x01;
+        expResp = 7;
+        sendCommand("FM_TUNE_STATUS", 0x22, 1);
+        break;
+
+    case 13:
+        Serial.print("Station: ");
+        Serial.println((((int)RESP[1]) << 8) | RESP[2]);
+        Serial.print("Waiting a few seconds");
+        wait(5);
+        Serial.println(" OK");
+        ready = true;
+        break;
+
+    case 14:
+        needCTS = true;
+        ARGS[0] = 0x01;
+        expResp = 7;
+        sendCommand("FM_TUNE_STATUS", 0x22, 1);
+        break;
+
+    case 15:
+        Serial.print("Station: ");
+        Serial.println((((int)RESP[1]) << 8) | RESP[2]);
+        Serial.print("Waiting a few seconds");
+        wait(5);
+        Serial.println(" OK");
+        ready = true;
+        break;
+
+    case 16:
+        Serial.print("Station: ");
+        Serial.println((((int)RESP[1]) << 8) | RESP[2]);
         Serial.println("All done");
+        sendCommand("POWER_DOWN", 0x11, 0);
         break;
-    }
-}
-
-void sleep(int millis)
-{
-    delay(millis >> CLOCK_SHIFT);
-}
-
-void wait(int seconds)
-{
-    for (int i = 0; i < seconds; ++i)
-    {
-        Serial.print('.');
-        sleep(1000);
     }
 }
 
@@ -255,7 +336,7 @@ void printBuffer(const char* name, const size_t size, const byte* buffer, int ra
     {
         for (int i = 0; i < size; ++i)
         {
-            if (radix == 16) 
+            if (radix == 16)
             {
                 Serial.print("0x");
                 if (buffer[i] < radix)
@@ -327,8 +408,14 @@ void prepareChip()
 
 
     Serial.print("Start the clock signal and wait 500ms for it to stabalize... ");
+    Serial.flush();
     pinMode(CLOCK_PIN, OUTPUT);
-    analogWrite(CLOCK_PIN, 128);
+    cli(); //stop interrupts
+    OCR1A = 4 >> CLOCK_SHIFT;
+    TCCR1A = (1 << WGM11);
+    TCCR1B = (1 << CS10);
+    TIMSK1 = (1 << OCIE1A);
+    sei(); //allow interrupts
     sleep(500);
     Serial.println("OK");
 
@@ -340,3 +427,26 @@ void prepareChip()
     Serial.println();
 }
 
+ISR(TIMER1_COMPA_vect)
+{
+    digitalWrite(CLOCK_PIN, clockPinState = !clockPinState);
+}
+
+int translateWB(int wb)
+{
+    return 64960 + (wb - 162400) / 25;
+}
+
+void sleep(int millis)
+{
+    delay(millis >> CLOCK_SHIFT);
+}
+
+void wait(int seconds)
+{
+    for (int i = 0; i < seconds; ++i)
+    {
+        Serial.print('.');
+        sleep(1000);
+    }
+}
